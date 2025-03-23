c204ad1d6247a77ddf1bf03bf102fc171172dc26
Mark Skelton <mdskelton99@gmail.com>
2025-03-23 07:03:43 -0500

    Close

--- src/content-script.js
+++ src/content-script.js
@@ -1,30 +1,106 @@
-const cache = new Map()
+const { a, div, span, style } = makeTags(["a", "div", "span", "style"])
 
-async function fetchCategories() {
-  if (cache.has("categories")) {
-    return cache.get("categories")
-  }
+const SINKING_FUNDS = [
+  1391931, // Roth IRAs
+  1433085, // Car Replacement
+]
 
-  const res = await fetch("https://api-beta.lunchmoney.app/categories", {
-    credentials: "include",
-  })
-
-  const json = await res.json()
-  const categories = json.assignable.map((category) => ({
-    ...category,
-    name: parseName(category.name),
-  }))
-
-  cache.set("categories", categories)
-  return categories
+const ACCOUNTS = {
+  ally: 238120,
+  loan: 154822,
 }
 
-/** @param {string} name */
+const ANIMATION = "bm-selector-observer"
+const ALLY = `.left > .card > .content > .card-content-wrapper:has(.account-sub-name [href*='account=${ACCOUNTS.ally}'])`
+
+class Cache {
+  #cache = new Map()
+
+  /**
+   * @param {string} key
+   * @param {T extends (key: string) => Promise<any>} fn
+   */
+  async get(key, fn) {
+    if (this.#cache.has(key)) {
+      return this.#cache.get(key)
+    }
+
+    const value = fn()
+    this.#cache.set(key, value)
+    return value
+  }
+}
+
+const cache = new Cache()
+
+async function get(url) {
+  return cache.get(url, async () => {
+    const res = await fetch(`https://api-beta.lunchmoney.app${url}`, {
+      method: "GET",
+      credentials: "include",
+    })
+
+    return res.json()
+  })
+}
+
+/**
+ * @returns {Promise<Map<number, any>>}
+ */
+function getCategories() {
+  return cache.get("#categories", async () => {
+    const res = await get("/categories")
+    const map = new Map()
+
+    function addToMap(category) {
+      map.set(category.id, {
+        ...category,
+        name: parseName(category.name),
+      })
+
+      if (category.children) {
+        category.children.forEach(addToMap)
+      }
+    }
+
+    res.nested.forEach(addToMap)
+
+    return map
+  })
+}
+
+/**
+ * @param {string} id
+ * @param {any} categories
+ */
+function findCategory(id, categories) {
+  const category = categories.flattened.find((cat) => cat.id === id)
+  if (!category) {
+    return null
+  }
+
+  if (category.group_id) {
+    const group = categories.nested.find((cat) => cat.id === category.group_id)
+    if (!group) {
+      return null
+    }
+
+    return group.children.find((cat) => cat.id === id)
+  }
+
+  return categories.nested.find((cat) => cat.id === id)
+}
+
+/**
+ * @param {string} name
+ */
 function parseName(name) {
   return new DOMParser().parseFromString(name, "text/html").body.textContent
 }
 
-/** @param {string} category */
+/**
+ * @param {string} category
+ */
 function buildURL(category) {
   const { pathname } = window.location
   const match = pathname.match(/\/budget\/(\d{4})\/(\d{2})\/(\d{2})/)
@@ -38,14 +114,14 @@ function buildURL(category) {
 }
 
 async function budgetURLs() {
-  const categories = await fetchCategories()
+  const categories = await getCategories()
   const cells = document.querySelectorAll(
     ".p-budget-table tbody tr:has(td[colspan='1']:first-child) td:nth-child(3) > div > div:first-child",
   )
 
   cells.forEach((cell) => {
     const categoryName = cell.textContent
-    const category = categories.find((cat) => cat.name === categoryName)
+    const category = categories.get(categoryName)
     if (!category) {
       return
     }
@@ -55,17 +131,31 @@ async function budgetURLs() {
       return
     }
 
-    const link = document.createElement("a")
-    link.classList.add("bm-cell")
-    link.href = url
-    link.textContent = categoryName
+    const link = a({ class: "bm-cell", href: url }, categoryName)
     cell.replaceChildren(link)
   })
 }
 
-const SINKING_FUNDS = ["Car Replacement", "Roth IRAs"]
+/**
+ * @param {number} amount
+ */
+function formatMoney(amount) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount)
+}
 
-function sinkingFunds() {
+async function getTotalSinkingFunds() {
+  const categories = await getCategories()
+  const totalAmount = SINKING_FUNDS.map(
+    (id) => categories.get(id)?.balance ?? 0,
+  ).reduce((a, b) => a + parseFloat(b), 0)
+
+  return totalAmount
+}
+
+async function sinkingFunds() {
   const divider = document.querySelector(
     ".right .ui.card .divider:last-of-type",
   )
@@ -73,25 +163,8 @@ function sinkingFunds() {
     return
   }
 
-  const rows = [
-    ...document.querySelectorAll(
-      ".p-budget-table tbody tr:has(td[colspan='1']:first-child)",
-    ),
-  ]
-
-  const totalAmount = rows
-    .filter((row) => SINKING_FUNDS.includes(row.children[2].textContent))
-    .map((row) => {
-      const rawValue = row.children[8].textContent
-      const value = parseFloat(rawValue.replace(/[$,]/g, ""))
-      return value
-    })
-    .reduce((a, b) => a + b, 0)
-
-  const formattedAmount = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(totalAmount)
+  const totalAmount = await getTotalSinkingFunds()
+  const formattedAmount = formatMoney(totalAmount)
 
   const sinkingFunds = document.querySelector("#sinking-funds")
   if (sinkingFunds) {
@@ -99,45 +172,112 @@ function sinkingFunds() {
     return
   }
 
-  const wrapper = document.createElement("div")
-  wrapper.className = "card-content-wrapper"
+  const node = div(
+    { class: "card-content-wrapper" },
+    div(
+      { class: "card-content no-wrap" },
+      span({ class: "card-text ellipsis font--bold" }, "Total Sinking Funds"),
+      span({ id: "sinking-funds", class: "card-number" }, formattedAmount),
+    ),
+  )
 
-  const content = document.createElement("div")
-  content.className = "card-content no-wrap"
-
-  const label = document.createElement("span")
-  label.className = "card-text ellipsis font--bold"
-  label.textContent = "Total Sinking Funds"
-
-  const amount = document.createElement("span")
-  amount.id = "sinking-funds"
-  amount.className = "card-number"
-  amount.textContent = formattedAmount
-
-  content.appendChild(label)
-  content.appendChild(amount)
-  wrapper.appendChild(content)
-  divider.after(wrapper)
+  divider.after(node)
 }
 
-const animation = "bm-selector-observer"
-
-function registerAnimation() {
-  if (cache.has(animation)) {
+/**
+ * @param {string} id
+ * @param {string} label
+ * @param {number} amount
+ */
+function insertAllyRow(id, label, amount) {
+  const formattedAmount = formatMoney(amount)
+  const amountId = `ally-${id}`
+  const existingNode = document.getElementById(amountId)
+  if (existingNode) {
+    existingNode.textContent = formattedAmount
     return
   }
 
-  const style = document.createElement("style")
-  style.textContent = `@keyframes ${animation} {}`
-  document.head.append(style)
-  cache.set(animation, true)
+  const node = div(
+    { class: "card-content-wrapper" },
+    div(
+      { class: "card-content no-wrap" },
+      span(
+        {
+          class: "card-text ellipsis",
+          style: "margin-left: 1rem",
+        },
+        div(
+          { class: "display--flex" },
+          span({ class: "hierarchy-line-icon-4" }),
+          span({ class: "account-sub-name" }, label),
+        ),
+      ),
+      span(
+        { class: "card-number" },
+        span({ class: "account-sub-amount", id: amountId }, formattedAmount),
+      ),
+    ),
+  )
+
+  document.querySelector(ALLY)?.after(node)
+}
+
+function formatCalendarDate(date) {
+  return date.toISOString().split("T")[0]
+}
+
+async function getSinkingFunds() {
+  const startDate = new Date()
+  startDate.setDate(1)
+
+  const endDate = new Date()
+  endDate.setMonth(startDate.getMonth() + 1)
+  endDate.setDate(endDate.getDate() - 1)
+
+  const params = new URLSearchParams({
+    start_date: formatCalendarDate(startDate),
+    end_date: formatCalendarDate(endDate),
+    include_exclude_from_budgets: "false",
+    include_occurrences: "true",
+    include_recurring: "true",
+  })
+
+  const res = await get(`/summary?${params.toString()}`)
+  return res.transactions.reduce((a, b) => a + b.amount, 0)
+}
+
+async function allySplit() {
+  const assets = await get("/assets")
+  const ally = parseFloat(
+    assets.find((asset) => asset.id === ACCOUNTS.ally).balance,
+  )
+  const loan = parseFloat(
+    assets.find((asset) => asset.id === ACCOUNTS.loan).balance,
+  )
+
+  const sinkingFunds = await getTotalSinkingFunds()
+  const reserved = loan + sinkingFunds
+  const available = ally - reserved
+
+  insertAllyRow("available-funds", "Available", available)
+  insertAllyRow("sinking-funds", "Reserved", reserved)
 }
 
 function init() {
-  observe(".p-budget-table:not(:has(.bm-cell)) tbody tr:first-of-type", () => {
-    budgetURLs()
-    sinkingFunds()
-  })
+  if (window.location.pathname.includes("/overview")) {
+    observe(ALLY, () => allySplit())
+  }
+
+  if (window.location.pathname.includes("/budget")) {
+    observe(
+      ".p-budget-table:not(:has(.bm-cell)) tbody tr:first-of-type",
+      () => {
+        budgetURLs()
+        sinkingFunds()
+      },
+    )
+  }
 }
 
 observe("h1", (h1) => {
@@ -154,20 +294,27 @@ observe("h1", (h1) => {
   })
 })
 
+function registerAnimation() {
+  return cache.get(ANIMATION, () => {
+    const rule = style(`@keyframes ${ANIMATION} {}`)
+    document.head.append(rule)
+  })
+}
+
 /**
  * @param {string} selector
- * @param {() => void} listener
+ * @param {(element: HTMLElement) => void} listener
  */
 function observe(selector, listener) {
   const seenMark = `bm-seen-${selector.replace(/[^a-zA-Z0-9]/g, "-")}`
   registerAnimation()
 
-  const rule = document.createElement("style")
-  rule.textContent = `
-    :where(${String(selector)}):not(.${seenMark}) {
-      animation: 1ms ${animation};
-    }
-  `
+  const rule = style(
+    `:where(${String(selector)}):not(.${seenMark}) {
+      animation: 1ms ${ANIMATION};
+    }`,
+  )
+
   document.body.prepend(rule)
 
   globalThis.addEventListener("animationstart", (event) => {
@@ -178,4 +325,57 @@ function observe(selector, listener) {
     event.target.classList.add(seenMark)
     listener(event.target)
   })
+}
+
+/**
+ * @param {string} tag
+ * @param {Record<string, string>} props
+ * @param {Node[]} children
+ * @returns {HTMLElement}
+ */
+function h(tag, props, children) {
+  const element = document.createElement(tag)
+
+  Object.entries(props).forEach(([key, value]) => {
+    element.setAttribute(key, value)
+  })
+
+  children.forEach((child) => element.append(child))
+
+  return element
+}
+
+/** @typedef {string | Element} Node */
+
+/**
+ * @typedef {{
+ *   (props: Record<string, string>, ...children: Node[]) => HTMLElement;
+ *   (...children: Node[]) => HTMLElement;
+ * }} TagFunction
+ */
+
+/**
+ * @param {string} tag
+ * @returns {TagFunction}
+ */
+function makeTag(tag) {
+  /** @type {TagFunction} */
+  return function (props, ...children) {
+    if (typeof props === "string" || props instanceof Element) {
+      return h(tag, {}, [props])
+    }
+
+    return h(tag, props, children)
+  }
+}
+
+/**
+ * @param {string[]} tags
+ * @returns {Record<string, TagFunction>}
+ */
+function makeTags(tags) {
+  return tags.reduce((acc, tag) => {
+    acc[tag] = makeTag(tag)
+    return acc
+  }, {})
 }
